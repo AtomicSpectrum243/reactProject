5e544150abdd6eb638ff5246a599177213ef3f56
AtomicSpectrum243 <7523494@gmail.com>
2022-06-22 10:52:49 +0700

    new commit

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
-import Logo from './components/Logo';
-import styles from './components/styles';
+import styles from './components/stySheet';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={[styles.title,styles.warning]}>Hello React Native</Text>
-      <Logo/>
+      <Text style={[styles.title]}>React Native</Text>
     </View>
   )
 }
