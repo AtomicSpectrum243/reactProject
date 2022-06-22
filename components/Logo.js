48c4aff2f5e7830118bef22bae6aac54b9d2536e
AtomicSpectrum243 <7523494@gmail.com>
2022-06-22 10:02:18 +0700

    new commit

--- components/Logo.js
+++ components/Logo.js
@@ -1,18 +1,19 @@
-import { StyleSheet } from 'react-native'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Logo = () => {
+    return (
+        <View>
+            <Text style={styles.textLogo}>TNI</Text>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      justifyContent:'center',
-      alignContent:'center'
-    },
-    title:{
-      fontSize:20,
-    },
-    warning:{
-      color:'blue'
+    textLogo:{
+        color:"blue",
+        fontSize:30
     }
-  });
-  
+})
 
-export default styles
+export default Logo
--- components/styles.js
+++ components/styles.js
@@ -7,10 +7,10 @@ const styles = StyleSheet.create({
       alignContent:'center'
     },
     title:{
-      fontSize:20,
+      fontSize:40,
     },
     warning:{
-      color:'blue'
+      color:'red'
     }
   });
   
