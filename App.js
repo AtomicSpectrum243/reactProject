340e127883d2fba8eb1f6fbafdf83c7a862688b2
Atomic_Spectrum <47299086+AtomicSpectrum243@users.noreply.github.com>
2022-07-27 10:27:56 +0700

    new commit 27-7-2022

--- App.js
+++ App.js
@@ -1,34 +1,16 @@
-import { StyleSheet, View, Text, Touchable } from "react-native";
-import React from "react";
-import Logo from "./components/Logo";
-import User from "./components/User";
-import LotsOfGreetings from "./components/LotsOfGreetings";
-import MyCustomTextWith from "./components/MyCustomTextWith";
-import Count from "./components/Count";
-import InputText from "./components/InputText";
-import UserNameandPassword from "./components/UserNameandPassword";
-import AlertComponents from "./components/AlertComponents";
-import ImageWithTextInput from "./components/ImageWithTextInput";
-import ButtonExample from "./components/ButtonExample";
-import Touchable_Example from "./components/Touchable_Example";
+import { View, Text, StyleSheet, Touchable } from 'react-native'
+import React from 'react'
+// import UserNamePassword from './components/UserNamePassword'
+// import AlertComponents from './components/AlertComponents'
+// import ImageWithTextInput from './components/ImageWithTextInput'
+// import ButtonExample from './components/ButtonExample'
+// import TouchableEx from './components/TouchableEx'
 import TouchablePractice from "./TouchablePractice";
 const App = () => {
-  return (
-    <view>
-      {/* <Logo /> */}
-      {/* <User /> */}
-      {/* <LotsOfGreetings /> */}
-      {/* <MyCustomTextWith /> */}
-      {/* <Count /> */}
-      {/* <InputText /> */}
-      {/* <UserNameandPassword/>   */}
-      {/* <UserNamePassword /> */}
-      {/* <AlertComponents /> */}
-      {/* <ImageWithTextInput/> */}
-      {/* <ButtonExample/> */}
-      <TouchablePractice/>
-    </view>
-  );
-};
-
-export default App;
+    return (
+      <View style={{flex:1}}>
+        <TouchablePractice/>
+      </View>
+    )
+  }
+  export default App
